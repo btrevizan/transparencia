9fa4c41f874ebf5fd472549f17a4d479d2e0c966
Emily Calvet <emilycalvet.inf@gmail.com>
2021-05-18 14:23:30 +0200

    feat(Frontend): add axis Graph for 2 dimentional query results

--- frontend/src/Components/Graph.js
+++ frontend/src/Components/Graph.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useStore } from "react-redux";
+import { useSelector } from "react-redux";
 import {
   VictoryBar,
   VictoryChart,
   VictoryAxis,
   VictoryTheme,
   VictoryGroup,
+  VictoryLabel,
+  VictoryStack,
 } from "victory";
 import logo from "../Style/Icons/transparency-color.png";
 
@@ -19,80 +21,125 @@ const colors = [
   "#F4AC32",
 ];
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
+
+const formatValue = (value) => {
+  if (value >= 1000000000000) {
+    return `${value / 1000000000000} T`;
+  } else if (value >= 1000000000) {
+    return `${value / 1000000000} B`;
+  } else if (value >= 1000000) {
+    return `${value / 1000000} M`;
+  } else {
+    return `${value / 1000} K`;
+  }
+};
+
+const groupBy = (key, array) =>
+  array.reduce((objectsByKeyValue, obj) => {
+    const value = obj[key];
+    objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+    return objectsByKeyValue;
+  }, {});
+
+const getTheme = (condition) => {
+  return condition ? VictoryTheme.material : VictoryTheme.grayscale;
+};
+const AxisGraph = (result) => {
+  const labelOnTop = result.length > 10;
+
+  return (
+    <VictoryChart
+      domainPadding={20}
+      // width={700}
+      // height={500}
+      theme={getTheme(labelOnTop)}
+      style={{ grid: { stroke: "none" } }}
+    >
+      <VictoryAxis
+        tickFormat={(x) => {
+          return labelOnTop ? "" : `${x}`;
+        }}
+      />
+      <VictoryAxis dependentAxis tickFormat={(y) => formatValue(y)} />
+      <VictoryBar
+        animate={{
+          duration: 2000,
+          easing: "bounce",
+        }}
+        style={{
+          data: { fill: colors[getRandomInt(0, 6)] },
+          labels: { fontSize: 10, fontWeight: "bold", fill: "white" },
+        }}
+        labels={({ datum }) => {
+          return labelOnTop ? `${datum.x}` : "";
+        }}
+        labelComponent={<VictoryLabel angle={-50} textAnchor="start" />}
+        data={result}
+        x="x"
+        y="y"
+      />
+    </VictoryChart>
+  );
+};
+
+const GroupGraph = (result) => {};
 
 const Graph = () => {
-  const store = useStore();
-  const storeResult = store.getState().result;
+  const storeResult = useSelector((store) => store.result);
   const [result, setResult] = useState(storeResult);
 
   useEffect(() => {
-    setResult([
-      { quarter: 1, earnings: 13000 },
-      { quarter: 2, earnings: 16500 },
-      { quarter: 3, earnings: 14250 },
-      { quarter: 4, earnings: 19000 },
-    ]);
+    if (storeResult && storeResult.length > 0) {
+      setResult(storeResult);
+    }
   }, [storeResult]);
 
   if (result) {
-    return (
-      <VictoryChart
-      // adding the material theme provided with Victory
-      // theme={VictoryTheme.material}
-      // domainPadding={10}
-      >
-        <VictoryGroup
-          offset={20}
-          colorScale={colors}
-          // categories={["2014", "2015", "2016", "2017"]}
-        >
-          <VictoryBar
-            data={[
-              { x: "org1", y: 1, label: "2014" },
-              { x: "org2", y: 2, label: "2015" },
-              { x: "org3", y: 5, label: "2016" },
-            ]}
-          />
-          <VictoryBar
-            data={[
-              { x: "org1", y: 2, label: "2016" },
-              { x: "org3", y: 1, label: "2014" },
-              { x: "org2", y: 7, label: "2014" },
-            ]}
-          />
-          <VictoryBar
-            data={[
-              { x: "org1", y: 3, label: "2017" },
-              { x: "org3", y: 4, label: "2015" },
-              { x: "org2", y: 9, label: "2016" },
-            ]}
-          />
-        </VictoryGroup>
-        {/* <VictoryAxis
-          tickValues={[1, 2, 3, 4]}
-          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-        />
-        <VictoryAxis dependentAxis tickFormat={(x) => `$${x / 1000}k`} />
-        <VictoryBar
-          animate={{
-            duration: 2000,
-            easing: "bounce",
-          }}
-          style={{
-            data: { fill: colors[getRandomInt(0, 6)], width: 25 },
-          }}
-          data={result}
-          x="quarter"
-          y="earnings"
-        />{" "}
-        */}
-      </VictoryChart>
-    );
+    if (!result[0].z) {
+      return AxisGraph(result);
+    } else {
+      return GroupGraph(result);
+    }
+    // return (
+    //   <VictoryChart
+    //     // adding the material theme provided with Victory
+    //     // theme={VictoryTheme.material}
+    //     domainPadding={20}
+    //   >
+    //     {/* <VictoryGroup
+    //       offset={20}
+    //       colorScale={colors}
+    //       // categories={["2014", "2015", "2016", "2017"]}
+    //     >
+    //       <VictoryBar
+    //         data={[
+    //           { x: "org1", y: 1, label: "2014" },
+    //           { x: "org2", y: 2, label: "2015" },
+    //           { x: "org3", y: 5, label: "2016" },
+    //         ]}
+    //       />
+    //       <VictoryBar
+    //         data={[
+    //           { x: "org1", y: 2, label: "2016" },
+    //           { x: "org3", y: 1, label: "2014" },
+    //           { x: "org2", y: 7, label: "2014" },
+    //         ]}
+    //       />
+    //       <VictoryBar
+    //         data={[
+    //           { x: "org1", y: 3, label: "2017" },
+    //           { x: "org3", y: 4, label: "2015" },
+    //           { x: "org2", y: 9, label: "2016" },
+    //         ]}
+    //       />
+    //     </VictoryGroup> */}
+    //   </VictoryChart>
+    // );
   } else {
     return <img src={logo} className="graph-logo" alt="logo" />;
   }
